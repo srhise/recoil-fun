0936ef741defd94a605ec72bd207499b591dc16c
Sean Hise <srhise@gmail.com>
2020-05-22 10:31:20 -0400

    fix import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 import { Square, Circle } from "react-feather";
-import { SquareShape } from "../components/SquareShape";
+import SquareShape from "../components/SquareShape";
 import Sidebar from "../components/Sidebar";
 /*  
 Things to keep track of
